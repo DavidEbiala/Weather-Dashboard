26bed3f0f738308ac6b0c15b01aca6febb8705c2
Zino Ebiala <eebia002@fiu.edu>
2023-05-11 23:25:12 -0400

    Changes to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var searchBtn = document.getElementById('search-btn');
 
 function getApi() {
     // replace `octocat` with anyone else's GitHub username
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=e113290cd8d9061903268496570d5a18';
+    var requestUrl = 
   
     fetch(requestUrl)
       .then(function (response) {
